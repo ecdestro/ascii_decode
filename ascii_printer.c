e8238cea16aad8346b00c0b69d99a5aeba83df1f
ecdestro <61299394+ecdestro@users.noreply.github.com>
2021-06-24 06:02:32 -0500

    Update ascii_printer.c

--- ascii_printer.c
+++ ascii_printer.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
 void printCharBits(char a) {
-	int i;
 	printf("bin: 0b");
-	for (i = 0; i < 8; i++) {
+	for (int i = 0; i < 8; i++) {
 		printf("%d", !!((a << i) & 0x80));	/* shift bits of char over by i bits, AND against 0b10000000, then double-negate to cast to bool */
 	}
 	printf("\n");
