3d05eeee8a33614d04b02c0253d06fcd88ba3174
ecdestro <61299394+ecdestro@users.noreply.github.com>
2021-06-24 04:19:53 -0500

    Update and rename ascii.c to ascii_printer.c

--- ascii_printer.c
+++ ascii_printer.c
@@ -1,29 +1,25 @@
 #include <stdio.h>
 
-int charToBits(char a);
+void printCharBits(char a);
 
-int main() {
+void main() {
 	int c;
 	while ((c = getchar()) != EOF) {		/* ctrl+d will send EOF character */
 		if (c != '\n') {
 			printf("bin: 0b");
-			charToBits(c);
+			printCharBits(c);
 			printf("dec: %d\n", c);
 			printf("hex: 0x%X\n", c);
 		}
 		else
 			;
 	}
-
-	return 0;
 }
 
-int charToBits(char a) {
+void printCharBits(char a) {
 	int i;
 	for (i = 0; i < 8; i++) {
 		printf("%d", !!((a << i) & 0x80));	/* shift bits of char over by i bits, AND against 0b10000000, then double-negate to cast to bool */
 	}
 	printf("\n");
-
-	return 0;
 }
